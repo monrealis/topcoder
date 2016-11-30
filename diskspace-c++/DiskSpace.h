2830eb4a10256facea20cc5d646606d7eda7c70d
Vytenis Bivainis <monrealis@gmail.com>
2016-11-30 15:50:04 +0200

    Pass the tests.

--- diskspace-c++/DiskSpace.h
+++ diskspace-c++/DiskSpace.h
@@ -1,11 +1,8 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <numeric>
-#include <set>
-#include <sstream>
-#include <string>
+#include <iterator>
 #include <vector>
+
 using namespace std;
 
 #define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
@@ -15,6 +12,30 @@ using namespace std;
 class DiskSpace {
 public:
 	int minDrives(vector<int> used, vector<int> total) {
-		return int();
+		sortDescending(total);
+		int spaceUsed = getTotalSpaceUsed(used);
+		return getNumberOfDisksNotEmpty(total, spaceUsed);
+	}
+
+private:
+	void sortDescending(vector<int>& integers) {
+		sort(integers.begin(), integers.end());
+		reverse(integers.begin(), integers.end());
+	}
+
+	int getTotalSpaceUsed(vector<int> used) {
+		int sum = 0;
+		FORIT(it, used)
+			sum += *it;
+		return sum;
+	}
+
+	int getNumberOfDisksNotEmpty(vector<int> sizesOfDisks, int spaceUsed) {
+		for (unsigned i = 0; i < sizesOfDisks.size(); ++i) {
+			if (spaceUsed == 0)
+				return i;
+			spaceUsed -= min(spaceUsed, sizesOfDisks.at(0));
+		}
+		return sizesOfDisks.size();
 	}
 };
