ed9358edbfbc47efc8ad4d68f8cc169970565666
Vytenis Bivainis <monrealis@gmail.com>
2016-12-04 00:49:46 +0200

    Change algorithm, not yet working.

--- ABBADiv1/src/ABBADiv1.java
+++ ABBADiv1/src/ABBADiv1.java
@@ -3,19 +3,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ABBADiv1 {
-    private int n;
     private int differenceOfAs;
     private int differenceOfBs;
-    private String initial;
-    private String target;
-    private boolean found;
 
     public String canObtain(String initial, String target) {
         findDifferences(initial, target);
-        this.initial = initial;
-        this.target = target;
-        collectPermutations("");
-        if (found)
+        if (differenceOfBs % 2 == 1)
+            initial = reverse(initial);
+        if (target.contains(initial))
             return "Possible";
         return "Impossible";
     }
@@ -23,37 +18,6 @@ public class ABBADiv1 {
     private void findDifferences(String initial, String target) {
         differenceOfAs = count(target, 'A') - count(initial, 'A');
         differenceOfBs = count(target, 'B') - count(initial, 'B');
-        n = differenceOfBs + differenceOfAs;
-    }
-
-
-    private void collectPermutations(String permutation) {
-        if (found)
-            return;
-        if (count(permutation, 'a') > differenceOfAs)
-            return;
-        if (count(permutation, 'b') > differenceOfBs)
-            return;
-        if (permutation.length() == n) {
-            if (target.equals(apply(initial, permutation))) {
-                found = true;
-                return;
-            }
-        } else
-            for (String action : new String[]{"a", "b"})
-                collectPermutations(permutation + action);
-    }
-
-    private String apply(String initial, String permutation) {
-        String r = initial;
-        for (char c : permutation.toCharArray())
-            if (c == 'a')
-                r += "A";
-            else {
-                r += "B";
-                r = reverse(r);
-            }
-        return r;
     }
 
     private String reverse(String text) {
@@ -63,7 +27,6 @@ public class ABBADiv1 {
             r[chars.length - 1 - i] = chars[i];
         return new String(r);
     }
-
 
     private int count(String target, char c) {
         int r = 0;
