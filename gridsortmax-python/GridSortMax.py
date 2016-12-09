91277d2687e4d4ba20eac03388d7f8a8428cd7b3
Vytenis Bivainis <monrealis@gmail.com>
2016-12-10 00:24:48 +0200

    Rename methods to match conventions.

--- gridsortmax-python/GridSortMax.py
+++ gridsortmax-python/GridSortMax.py
@@ -2,10 +2,10 @@ class GridSortMax:
     def findMax(self, n, m, grid):
         self.n = n
         self.m = m
-        rows, columns = self.findOrder(n, m, grid)
-        return self.compareWithPerfect(self.swap(grid, columns, rows))
+        rows, columns = self.find_order(n, m, grid)
+        return self.compare_with_perfect(self.swap(grid, columns, rows))
 
-    def findOrder(self, n, m, grid):
+    def find_order(self, n, m, grid):
         return self.OrderFinder(n, m, grid).find()
 
     class OrderFinder:
@@ -17,78 +17,78 @@ class GridSortMax:
             self.grid = grid
 
         def find(self):
-            for value in range(1, self.getCellCount() + 1):
-                self.checkValue(value)
-            self.fillEmptyRowsAndColumns()
+            for value in range(1, self.get_cell_count() + 1):
+                self.add_next_value(value)
+            self.fill_empty_rows_and_columns()
             return self.rows, self.columns
 
-        def checkValue(self, value):
-            (expectedRow, expectedColumn) = self.getCoordinates(value - 1)
-            (currentRow, currentColumn) = self.findCurrentCoordinates(value)
+        def add_next_value(self, value):
+            (expectedRow, expectedColumn) = self.get_coordinates(value - 1)
+            (currentRow, currentColumn) = self.find_current_coordinates(value)
             if self.rows[expectedRow] not in (None, currentRow):
                 return
             if self.columns[expectedColumn] not in (None, currentColumn):
                 return
-            self.addRow(currentRow, expectedRow)
-            self.addColumn(currentColumn, expectedColumn)
+            self.add_row(currentRow, expectedRow)
+            self.add_column(currentColumn, expectedColumn)
 
-        def addRow(self, currentRow, expectedRow):
+        def add_row(self, currentRow, expectedRow):
             if not self.rows.__contains__(currentRow):
                 self.rows[expectedRow] = currentRow
                 self.remainingRows.remove(currentRow)
 
-        def addColumn(self, currentColumn, expectedColumn):
+        def add_column(self, currentColumn, expectedColumn):
             if not self.columns.__contains__(currentColumn):
                 self.columns[expectedColumn] = currentColumn
                 self.remainingColumns.remove(currentColumn)
 
-        def fillEmptyRowsAndColumns(self):
-            self.fillEmptyIndexes(self.rows, self.remainingRows)
-            self.fillEmptyIndexes(self.columns, self.remainingColumns)
+        def fill_empty_rows_and_columns(self):
+            self.fill_empty_indexes(self.rows, self.remainingRows)
+            self.fill_empty_indexes(self.columns, self.remainingColumns)
 
-        def getCoordinates(self, value):
-            return (value / self.getM(), value % self.getM())
+        def get_coordinates(self, value):
+            return (value / self.get_m(), value % self.get_m())
 
-        def fillEmptyIndexes(self, all, remaining):
+        def fill_empty_indexes(self, all, remaining):
             empty = [i for i, val in enumerate(all) if val == None]
             for i, index in enumerate(empty):
                 all[index] = remaining[i]
 
-        def findCurrentCoordinates(self, value):
-            return self.getCoordinates(self.grid.index(value))
+        def find_current_coordinates(self, value):
+            return self.get_coordinates(self.grid.index(value))
 
-        def getCellCount(self):
-            return self.getN() * self.getM()
+        def get_cell_count(self):
+            return self.get_n() * self.get_m()
 
-        def getN(self):
+        def get_n(self):
             return self.rows.__len__()
 
-        def getM(self):
+        def get_m(self):
             return self.columns.__len__()
 
     def swap(self, grid, columns, rows):
-        rowsSwapped = self.swapRows(grid, rows)
-        columnsSwapped = self.swapColumns(rowsSwapped, columns)
+        rowsSwapped = self.swap_rows(grid, rows)
+        columnsSwapped = self.swap_columns(rowsSwapped, columns)
         return columnsSwapped
 
-    def swapRows(self, grid, rows):
+    def swap_rows(self, grid, rows):
         source = lambda row, column: row * self.m + column
         target = lambda row, column: rows[row] * self.m + column
-        return self.swapCells(grid, source, target)
+        return self.swap_cells(grid, source, target)
 
-    def swapColumns(self, grid, columns):
+    def swap_columns(self, grid, columns):
         source = lambda row, column: row * self.m + column
         target = lambda row, column: row * self.m + columns[column]
-        return self.swapCells(grid, source, target)
+        return self.swap_cells(grid, source, target)
 
-    def swapCells(self, grid, source, target):
+    def swap_cells(self, grid, source, target):
         r = [None] * grid.__len__()
         for row in range(self.n):
             for column in range(self.m):
                 r[source(row, column)] = grid[target(row, column)]
         return r
 
-    def compareWithPerfect(self, grid):
+    def compare_with_perfect(self, grid):
         r = ""
         for i in range(grid.__len__()):
             r += "1" if grid[i] == i + 1  else "0"
