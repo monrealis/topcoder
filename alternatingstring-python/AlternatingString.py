67e1fc2884777bcd8042a687378beb04dc490ae4
Vytenis Bivainis <monrealis@gmail.com>
2016-12-16 00:39:14 +0200

    Pass the tests.

--- alternatingstring-python/AlternatingString.py
+++ alternatingstring-python/AlternatingString.py
@@ -1,3 +1,8 @@
 class AlternatingString:
     def maxLength(self, s):
-        return None
+        extended = ' ' + s + ' '
+        streaks = [0]
+        for i in range(1, len(extended) - 1):
+            differs_from_previous = extended[i] != extended[i - 1]
+            streaks.append(streaks[i - 1] + 1 if differs_from_previous else 1)
+        return max(streaks)
