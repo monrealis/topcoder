7d40254378255f18bea0b3c708db956165379eac
Vytenis Bivainis <monrealis@gmail.com>
2016-12-31 01:47:34 +0200

    Refactor.

--- dagconstruction-python/DAGConstruction.py
+++ dagconstruction-python/DAGConstruction.py
@@ -7,27 +7,29 @@ class DAGConstruction:
         self.remaining_indexes = list(range(len(x)))
         self.edges = []
         self.x = x;
-        return self.run()
+        return self.inspect_remaining()
 
-    def run(self):
+    def inspect_remaining(self):
         while self.remaining_indexes:
-            index = self.take_next_index()
-            reaching = self.get_reaching(index, self.edges)
-            delta = self.x[index] - reaching
-            if delta != 0:
-                r = range(len(self.x))
-                to_nodes_combinations = list(itertools.combinations(r, delta))
-                print(to_nodes_combinations)
-                for to_nodes in to_nodes_combinations:
-                    if self.loop_would_exist(index, to_nodes):
-                        continue
-                    newEdges = [];
-                    for t in to_nodes:
-                        newEdges.append((index, t));
-                    reaching_with_edge = self.get_reaching(index, self.edges + newEdges)
-                    if reaching_with_edge == self.x[index]:
-                        self.edges.extend(newEdges)
+            self.inspect_next_node(self.take_next_index())
         return self.create_result()
+
+    def inspect_next_node(self, from_node):
+        reaching = self.get_reaching(from_node, self.edges)
+        delta = self.x[from_node] - reaching
+        if delta != 0:
+            r = range(len(self.x))
+            to_nodes_combinations = list(itertools.combinations(r, delta))
+            print(to_nodes_combinations)
+            for to_nodes in to_nodes_combinations:
+                if self.loop_would_exist(from_node, to_nodes):
+                    continue
+                new_edges = [];
+                for t in to_nodes:
+                    new_edges.append((from_node, t));
+                reaching_with_edge = self.get_reaching(from_node, self.edges + new_edges)
+                if reaching_with_edge == self.x[from_node]:
+                    self.edges.extend(new_edges)
 
     def loop_would_exist(self, from_node, to_nodes):
         if from_node in to_nodes:
